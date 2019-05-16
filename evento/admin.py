f4b836f8bc376019d5749c7de879ace4b2905a19
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-15 22:40:17 -0300

    feat: criação dos models de evento e itens

--- evento/admin.py
+++ evento/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Evento, Item
+from django.contrib.auth.models import User
+from rest_framework.authtoken.admin import TokenAdmin
 # Register your models here.
+
+admin.site.register(Evento)
+admin.site.register(Item)
+TokenAdmin.raw_id_fields = ('user',)
--- evento/models.py
+++ evento/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    descricao = models.TextField(blank=True)
+    url_image = models.TextField(default='https://www.mariowiki.com/images/thumb/a/a6/Super_Mushroom_Artwork_-_Super_Mario_3D_World.png/1200px-Super_Mushroom_Artwork_-_Super_Mario_3D_World.png')
+
+    def __str__(self):
+        return self.name
+
+class Evento(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    is_active = models.BooleanField(default=False)
+    multiplicador_xp = models.FloatField(blank=True, default=1)
+    data_inicio = models.DateTimeField(blank=False)
+    data_final = models.DateTimeField(blank=True)
+    primeiro = models.ForeignKey(Item, blank=True, on_delete=models.PROTECT, related_name='Item1')
+    segundo = models.ForeignKey(Item, blank=True, on_delete=models.PROTECT, related_name='Item2')
+    terceiro = models.ForeignKey(Item, blank=True, on_delete=models.PROTECT, related_name='Item3')
+    quarto = models.ForeignKey(Item, blank=True, on_delete=models.PROTECT, related_name='Item4')
+    quinto =models.ForeignKey(Item, blank=True, on_delete=models.PROTECT, related_name='Item5')
+    sexto = models.ForeignKey(Item, blank=True, on_delete=models.PROTECT, related_name='Item6')
+
+    def __str__(self):
+        return 'Nome: ' + self.name
