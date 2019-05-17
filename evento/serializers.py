bc93186c0bf1a75b654ade0b870615541aadb7b2
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-16 23:03:00 -0300

    feat: endpoint para atualizar um item

--- evento/serializers.py
+++ evento/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 from evento.models import *
 
-class ItemSerializer(serializers.Serializer):
+class ItemSerializer(serializers.ModelSerializer):
+    id = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all())
+    name = serializers.CharField(required=True, max_length=200)
+    descricao = serializers.CharField(required=False)
+    url_image = serializers.CharField(required=False)
+
     class Meta:
         model = Item
-        fields = ["__all__"]
+        fields = ('id', 'name', 'descricao', 'url_image',)
 
     def create(self, validated_data):
         return Item.objects.create(**validated_data)
 
-class EventoSerializer(serializers.Serializer):
+class EventoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Evento
         fields = ["__all__"]
--- evento/views.py
+++ evento/views.py
@@ -8,14 +8,21 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from rest_framework import permissions
+from rest_framework.renderers import JSONRenderer
 
 from evento.serializers import EventoSerializer, ItemSerializer
 from evento.models import Evento, Item
+
+from django.contrib.auth.models import User
+from jogador.models import Jogador
 
 class ItemViewSet(GenericViewSet):
 
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    lookup_field = 'pk'
+    permission_classes = (permissions.IsAuthenticated,)
     @method_decorator(csrf_exempt)
     @action(methods=['POST'], detail=False, url_path='criarItem')
     def CriarItem(self, request):
@@ -26,19 +33,62 @@ class ItemViewSet(GenericViewSet):
         else:
             item = request.POST.get('item')
 
-        cadastrado = Item.objects.filter(name=item['name'])
+        if(Jogador.objects.get(user=request.user).tipo == 'A'):
+            cadastrado = Item.objects.filter(name=item['name'])
 
-        if cadastrado.exists():
-            return Response(
-                {"error": True, "message": "Esse Objeto existe."},
-                status=400)
+            if cadastrado.exists():
+                return Response(
+                    {"error": True, "message": "Esse Objeto existe."},
+                    status=400)
+            else:
+                newItem = Item()
+                newItem.name = item['name']
+                newItem.descricao = item['descricao']
+                newItem.url_image = item['url_image']
+
+                newItem.save()
+
+                serializer = ItemSerializer(newItem)
+                return Response(serializer.data, status=200)
         else:
-            newItem = Item()
-            newItem.name = item['name']
-            newItem.descricao = item['descricao']
-            newItem.url_image = item['url_image']
+            return Response(
+                {"error": True, "message": "Você não tem permissão para criar um item."},
+                status=400)
 
-            newItem.save()
+    @method_decorator(csrf_exempt)
+    @action(methods=['GET'], detail=False, url_path='consultarItens')
+    def consultarItens(self, request):
+        itens = Item.objects.all()
+        serializer = ItemSerializer(itens, many=True)
+        return Response({"list": serializer.data})
 
-            serializer = ItemSerializer(newItem)
-            return Response(serializer.data, status=200)
+    @method_decorator(csrf_exempt)
+    @action(methods=['PUT'], detail=False, url_path='atualizarItem')
+    def atualizarItem(self, request, pk=None):
+        if (request.META['CONTENT_TYPE'] == 'application/json'):
+            jsonData = json.loads(request.body)
+
+            item = jsonData
+        else:
+            item = request.POST.get('item')
+
+        if (Jogador.objects.get(user=request.user).tipo == 'A'):
+            itemFinded = Item.objects.get(pk=item['id'])
+
+            if not(itemFinded.name != item['name'] and Item.objects.filter(name=item['name'])):
+                itemFinded.name = item['name']
+                itemFinded.descricao = item['descricao']
+                itemFinded.urlImage = item['url_image']
+                itemFinded.save()
+
+                serializer = ItemSerializer(itemFinded)
+                return Response({'Item:': serializer.data}, status=200)
+            else:
+                return Response(
+                    {"message": "Esse nome ja esta em uso."},
+                    status=400)
+
+        else:
+            return Response(
+                {"message": "Você não tem permissão para criar um item."},
+                status=400)
