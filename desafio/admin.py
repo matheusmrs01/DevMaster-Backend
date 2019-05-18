c58d857ccc86269bfdd12af371b09b197fc36be7
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-18 02:38:34 -0300

    feat: criação do model de desafio e adição dele ao admin

--- desafio/admin.py
+++ desafio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Desafio, DesafioMissoes
+from rest_framework.authtoken.admin import TokenAdmin
 # Register your models here.
+
+admin.site.register(Desafio)
+admin.site.register(DesafioMissoes)
+TokenAdmin.raw_id_fields = ('user',)
--- desafio/models.py
+++ desafio/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from jogador.models import Jogador
+from evento.models import Item
+from missao.models import Missao
 
 # Create your models here.
+
+class Desafio(models.Model):
+    STATUS_CHOICES = (
+        ('P', 'Proposta'),
+        ('A', 'Aposta'),
+        ('C', 'Conslusão'),
+    )
+
+    nome = models.CharField(max_length=200, verbose_name='Nome Desafio', blank=False)
+    desafiante = models.ForeignKey(Jogador, blank=True, on_delete=models.PROTECT, related_name='Desafiante')
+    desafiado = models.ForeignKey(Jogador, blank=True, on_delete=models.PROTECT, related_name='Desafiado')
+    status = models.CharField(verbose_name='Status', max_length=1, choices=STATUS_CHOICES, default='P')
+    item_desafiante = models.ForeignKey(Item, blank=True, on_delete=models.PROTECT, related_name='ItemDesafiante')
+    item_desafiado = models.ForeignKey(Item, blank=True, on_delete=models.PROTECT, related_name='ItemDesafiado')
+
+    def __str__(self):
+        return 'Desafio: ' + self.name + ' - Entre: ' + self.desafiante.user.get_full_name() + ' VS ' + self.desafiado.user.get_full_name()
+
+class DesafioMissoes(models.Model):
+    desafio = models.ForeignKey(Desafio, blank=False, on_delete=models.PROTECT, related_name='Desafio')
+    missao = models.ForeignKey(Missao, blank=False, on_delete=models.PROTECT, related_name='Missão')
+
+    def __str__(self):
+        return 'Desafio: ' + self.desafio.name + ' - Entre: ' + self.desafio.desafiante.user.get_full_name() + ' VS ' + self.desafio.desafiado.user.get_full_name()
