fb4678924978bf5826402bcbfccaf3162bd20a30
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-18 04:12:28 -0300

    fix: item do desafiado e desafiado agora pode receber null

--- desafio/models.py
+++ desafio/models.py
@@ -14,17 +14,17 @@ class Desafio(models.Model):
 
     nome = models.CharField(max_length=200, verbose_name='Nome Desafio', blank=False)
     desafiante = models.ForeignKey(Jogador, blank=True, on_delete=models.PROTECT, related_name='Desafiante')
-    desafiado = models.ForeignKey(Jogador, blank=True, on_delete=models.PROTECT, related_name='Desafiado')
+    desafiado = models.ForeignKey(Jogador, blank=True, null=True, on_delete=models.PROTECT, related_name='Desafiado')
     status = models.CharField(verbose_name='Status', max_length=1, choices=STATUS_CHOICES, default='P')
     item_desafiante = models.ForeignKey(JogadorItem, blank=True, on_delete=models.PROTECT, related_name='ItemDesafiante')
-    item_desafiado = models.ForeignKey(JogadorItem, blank=True, on_delete=models.PROTECT, related_name='ItemDesafiado')
+    item_desafiado = models.ForeignKey(JogadorItem, blank=True, null=True, on_delete=models.PROTECT, related_name='ItemDesafiado')
 
     def __str__(self):
-        return 'Desafio: ' + self.name + ' - Entre: ' + self.desafiante.user.get_full_name() + ' VS ' + self.desafiado.user.get_full_name()
+        return 'Desafio: ' + self.nome + ' - Entre: ' + self.desafiante.user.get_full_name()
 
 class DesafioMissoes(models.Model):
     desafio = models.ForeignKey(Desafio, blank=False, on_delete=models.PROTECT, related_name='Desafio')
     missao = models.ForeignKey(Missao, blank=False, on_delete=models.PROTECT, related_name='Missão')
 
     def __str__(self):
-        return 'Desafio: ' + self.desafio.name + ' - Entre: ' + self.desafio.desafiante.user.get_full_name() + ' VS ' + self.desafio.desafiado.user.get_full_name()
+        return 'Desafio: ' + self.desafio.nome + ' - Entre: ' + self.desafio.desafiante.user.get_full_name()
