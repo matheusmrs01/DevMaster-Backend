8d4d8f6458e97f0a93ec32cbbac8d5587229b68a
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-19 03:50:16 -0300

    feat: adicionado XPEvento ao admin

--- evento/admin.py
+++ evento/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Evento, Item
+from jogador.models import XpEvento
 from django.contrib.auth.models import User
 from rest_framework.authtoken.admin import TokenAdmin
 # Register your models here.
 
 admin.site.register(Evento)
 admin.site.register(Item)
+admin.site.register(XpEvento)
 TokenAdmin.raw_id_fields = ('user',)
