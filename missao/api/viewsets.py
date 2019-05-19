cd8442c9ca9bf19effe412465a6b6a2b40f278ed
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-19 03:50:46 -0300

    feat: ao fechar a missão ja é somado a xp evento, se tiver

--- missao/api/viewsets.py
+++ missao/api/viewsets.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,9 +10,11 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404
 from django.conf import settings
 
-from jogador.models import Jogador
+from jogador.models import Jogador, XpEvento
 from missao.models import Missao
 from django.contrib.auth.models import User
+
+from evento.models import Evento
 
 from .serializers import MissaoSerializer, CriarMissaoSerializer
 
@@ -38,9 +42,47 @@ class MissaoViewSet(mixins.ListModelMixin,
 
 
     def update(self, request, pk, *args, **kwargs):
-        missao = Missao.objects.get(id=pk)
-        if missao.jogador.user == request.user:
-            return super(MissaoViewSet, self).update(request, *args, **kwargs)
+        if (request.META['CONTENT_TYPE'] == 'application/json'):
+            jsonData = json.loads(request.body)
+
+            missao = jsonData
+        else:
+            missao = request.POST.get('desafio', '')
+
+        missaoFinded = Missao.objects.get(id=pk)
+        if missaoFinded.jogador.user == request.user:
+            missaoFinded.status = True
+            missaoFinded.nice_tempo = missao['nice_tempo']
+            if missao['nice_tempo'] == False:
+                missaoFinded.xp_missao = missao['xp_missao']
+
+            aux_xp = 0
+
+            if Evento.objects.filter(is_active=True):
+                eventos = Evento.objects.filter(is_active=True)
+
+                for evento in eventos:
+                    aux_xp = aux_xp + (missaoFinded.xp_missao * evento.multiplicador_xp)
+
+                    if XpEvento.objects.filter(evento=evento,jogador=missaoFinded.jogador):
+                        xpEventoFinded = XpEvento.objects.get(evento=evento,jogador=missaoFinded.jogador)
+                        xpEventoFinded.xp_evento = xpEventoFinded.xp_evento + (missaoFinded.xp_missao * evento.multiplicador_xp)
+                        xpEventoFinded.save()
+                    else:
+                        newXpEvento = XpEvento()
+                        newXpEvento.evento = evento
+                        newXpEvento.jogador = missaoFinded.jogador
+                        newXpEvento.xp_evento = missaoFinded.xp_missao * evento.multiplicador_xp
+                        newXpEvento.save()
+
+            else:
+                aux_xp = aux_xp + missaoFinded.xp_missao
+
+            missaoFinded.xp_ganha = aux_xp
+            missaoFinded.save()
+
+            serializer = MissaoSerializer(missaoFinded)
+            return Response({'Missão atualizada com sucesso.'})
         else:
             return Response({'Você não é dono desta missão!'})
 
