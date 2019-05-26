710058345d636c1ff1af2a9c565648b6d537b81e
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-26 14:40:33 -0300

    fix: alteração no model de Missãogrupo

--- missao/admin.py
+++ missao/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Missao, MissãoGrupo
+from .models import Missao, Grupo
 from django.contrib.auth.models import User
 from rest_framework.authtoken.admin import TokenAdmin
 # Register your models here.
 
 admin.site.register(Missao)
-admin.site.register(MissãoGrupo)
+admin.site.register(Grupo)
 TokenAdmin.raw_id_fields = ('user',)
--- missao/models.py
+++ missao/models.py
@@ -9,18 +9,21 @@ class Missao(models.Model):
     xp_missao = models.FloatField(blank=False)
     data = models.CharField(max_length=100, default='', blank=True)
     nice_tempo = models.BooleanField(default=False, blank=False)
+    nice_data = models.BooleanField(default=False, blank=False)
     status = models.BooleanField(default=False, blank=False)
     id_issue = models.IntegerField(default=0, blank=False)
     id_projeto = models.IntegerField(default=0, blank=False)
+    id_milestone = models.IntegerField(default=None, null=True, blank=True) 
 
     def __str__(self):
         return 'Jogador: ' + self.jogador.user.get_full_name() + ' - Missão: ' + self.nome_missao
 
-class MissãoGrupo(models.Model):
-    missao = models.ForeignKey(Missao, on_delete=models.CASCADE, related_name='MissãoGrupo')
+class Grupo(models.Model):
     name = models.CharField(blank=True, max_length=200)
-    xp_missao = models.FloatField(blank=False)
+    xp_grupo = models.FloatField(blank=False)
     status = models.BooleanField(default=False, blank=False)
+    id_milestone = models.IntegerField(default=0, blank=False) 
+    participantes = models.CharField(max_length=500, default='jogador', blank=False)
 
     def __str__(self):
-        return 'Missão: ' + self.missao.name + ' - Name: ' + self.name
+        return 'Missão: ' + self.name + ' - xp_grupo: ' + str(self.xp_grupo)
