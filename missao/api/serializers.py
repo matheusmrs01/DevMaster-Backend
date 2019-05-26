68a8af28cc83dc3ec0ff35a779349f0ed1b265f1
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-26 14:41:28 -0300

    feat: criação dos endpoints de grupo

--- missao/api/serializers.py
+++ missao/api/serializers.py
@@ -1,20 +1,25 @@
 from rest_framework import serializers
-from missao.models import Missao
+from missao.models import Missao, Grupo
 from jogador.models import Jogador
+from jogador.api.serializers import JogadorSerializer
 
 
 class MissaoSerializer(serializers.ModelSerializer):
     id = serializers.PrimaryKeyRelatedField(queryset=Missao.objects.all())
-    jogador = serializers.PrimaryKeyRelatedField(queryset=Jogador.objects.all(), many=False)
+    jogador = JogadorSerializer()
 
     class Meta:
         model = Missao
-        fields = ('id', 'jogador', 'nome_missao', 'xp_missao', 'data', 'nice_tempo', 'status', 'id_issue', 'id_projeto')
+        fields = ('id', 'jogador', 'nome_missao', 'xp_missao', 'data', 'nice_tempo', 'nice_data', 'xp_ganha', 'status', 'id_issue', 'id_projeto', 'id_milestone')
 
 class CriarMissaoSerializer(serializers.ModelSerializer):
     jogador = serializers.PrimaryKeyRelatedField(queryset=Jogador.objects.all(), many=False)
 
     class Meta:
         model = Missao
-        fields = ('jogador', 'nome_missao', 'xp_missao', 'data', 'status', 'nice_tempo', 'id_issue', 'id_projeto')
+        fields = ('jogador', 'nome_missao', 'xp_missao', 'data', 'status', 'nice_tempo', 'id_issue', 'id_projeto', 'id_milestone')
 
+class GrupoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grupo
+        fields = '__all__'
--- missao/views.py
+++ missao/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+import base64
+import imghdr
+import json
 
-# Create your views here.
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import permissions
+from rest_framework.renderers import JSONRenderer
+
+from missao.api.serializers import GrupoSerializer, MissaoSerializer
+from missao.models import Grupo, Missao
+
+class GrupoViewSet(GenericViewSet):
+    queryset = Grupo.objects.all()
+    serializer_class = GrupoSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    @method_decorator(csrf_exempt)
+    @action(methods=['GET'], detail=False, url_path='listarGrupos')
+    def ListarGrupos(self, request):
+        grupos = Grupo.objects.all()
+        serializer = GrupoSerializer(grupos, many=True)
+
+        return Response({'List': serializer.data})
+
+    @method_decorator(csrf_exempt)
+    @action(methods=['GET'], detail=False, url_path='consultarGrupo/(?P<pk>[0-9]+)$') 
+    def ConsultarGrupos(self, request, pk=None):
+        grupo = Grupo.objects.get(id=pk)
+        serializer = GrupoSerializer(grupo)
+
+        return Response({'Grupo': serializer.data})
+    
+    @method_decorator(csrf_exempt)
+    @action(methods=['GET'], detail=False, url_path='missoes/(?P<pk>[0-9]+)$') 
+    def missoes(self, request, pk=None):
+        grupo = Grupo.objects.get(id=pk)
+        missoes = Missao.objects.filter(id_milestone=grupo.id_milestone)
+        print(missoes)
+        serializer = MissaoSerializer(missoes, many=True)
+
+        return Response({'Missoes': serializer.data})
