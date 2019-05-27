25a761f318e2d4ac5d61365206961002fc38e40c
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-27 00:25:45 -0300

    fix: campo add no serializer de jogador

--- jogador/api/serializers.py
+++ jogador/api/serializers.py
@@ -103,7 +103,7 @@ class JogadorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Jogador
-        fields = ('id', 'user', 'private_token', 'xp_total', 'm_realizadas', 'm_adquiridas', 'mr_nadata', 'url_imagem', 'desafios_v', 'eventos_v')
+        fields = ('id', 'user', 'private_token', 'xp_total', 'm_realizadas', 'm_adquiridas', 'mr_nadata', 'url_imagem', 'desafios_v', 'eventos_v', 'tipo')
 
 
 class JogadorItemSerializer(serializers.ModelSerializer):
