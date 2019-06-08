8099ace7f1af4869f86d70a2b2243916e61d3359
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-06-08 02:48:24 -0300

    fix: atualização no arquivo de rotas

--- DevMaster_Backend/urls.py
+++ DevMaster_Backend/urls.py
@@ -24,22 +24,30 @@ from missao.api.viewsets import CriarMissaoViewSet, MissaoViewSet
 from missao.views import GrupoViewSet
 from evento.views import ItemViewSet, EventoViewSet
 from desafio.views import DesafioViewSet,DesafioMissaoViewSet
-from burndown.views import BurndownViewSet, MissaoBurndownViewSet
+
+
+from burndown.views import BurndownViewSet
+# , MissaoBurndownViewSet
+
+from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'jogador', JogadorViewSet)
+router.register(r'jogador', JogadorViewSet, base_name='jogador')
 router.register(r'jogadorxpevento', XpeventoViewSet, base_name='jogadorxpevento')
 router.register(r'jogadoritens', JogadorItemViewSet, base_name='jogadoritens')
-router.register(r'criarjogador', JogadorCreateViewSet)
-router.register(r'criarmissao', CriarMissaoViewSet)
-router.register(r'missao', MissaoViewSet)
+router.register(r'criarjogador', JogadorCreateViewSet, base_name='jogadorcreate')
+router.register(r'criarmissao', CriarMissaoViewSet, base_name='missaocreate')
+router.register(r'missao', MissaoViewSet, base_name='missao')
 router.register(r'grupo', GrupoViewSet, base_name='grupo')
 router.register(r'item', ItemViewSet, base_name='item')
 router.register(r'evento', EventoViewSet, base_name='evento')
 router.register(r'desafio', DesafioViewSet, base_name='desafio')
 router.register(r'missaodesafio', DesafioMissaoViewSet, base_name='missaodesafio')
+
+
 router.register(r'burndown', BurndownViewSet, base_name='burndown')
-router.register(r'missaoburndown', MissaoBurndownViewSet, base_name='missaoburndown')
+
+# router.register(r'missaoburndown', MissaoBurndownViewSet, base_name='missaoburndown')
 
 
 urlpatterns = [
