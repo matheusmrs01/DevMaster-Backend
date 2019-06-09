c957a2c06a17ef49dd336f1b144f59dde534857b
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-06-09 04:22:12 -0300

    fix: trocando iten do desafio

--- desafio/views.py
+++ desafio/views.py
@@ -80,24 +80,19 @@ class DesafioViewSet(GenericViewSet):
 
 
     @method_decorator(csrf_exempt)
-    @action(methods=['DELETE'], detail=False, url_path='deletarDesafio')
-    def ExcluirDesafio(self, request):
-        if (request.META['CONTENT_TYPE'] == 'application/json'):
-            jsonData = json.loads(request.body)
-
-            desafio = jsonData
-        else:
-            desafio = request.POST.get('desafio', '')
-
+    @action(methods=['DELETE'], detail=False, url_path='deletarDesafio/(?P<pk>[0-9]+)$')
+    def ExcluirDesafio(self, request, pk=None):
         try:
-            desafioFinded = Desafio.objects.get(pk=desafio['id'])
+            desafioFinded = Desafio.objects.get(pk=pk)
 
-            if Jogador.objects.get(user=request.user) == desafioFinded.desafiante:
-                desafioFinded.delete()
-
-                return Response({"message": "Desafio deletado com sucesso."}, status=200)
+            if Jogador.objects.get(user=request.user) == desafioFinded.desafiante or Jogador.objects.get(user=request.user) == desafioFinded.desafiado:
+                if desafioFinded.status == 'P':
+                    desafioFinded.delete()
+                    return Response({"message": "Desafio deletado com sucesso."}, status=200)
+                else:
+                    return Response({"error": True, "message":"O desafio não esta mais em fase de proposta."})
             else:
-                return Response({"error": True, "message": "Você não é o desafiante do desafio."}, status=400)
+                return Response({"error": True, "message": "Você não é nem o desafiante, e nem o desafiado, do desafio."}, status=400)
         except Desafio.DoesNotExist:
             return Response({"error": True, "message": "Desafio não existe."}, status=400)
 
@@ -120,125 +115,155 @@ class DesafioViewSet(GenericViewSet):
                     if jogadorFinded == desafioFinded.desafiado:
                         #quando o jogador vai adicionar o item no desafio pela primeira vez
                         if not desafioFinded.item_desafiado:
-                            if JogadorItem.objects.get(pk=int(desafio['item'])):
-                                jogador_item = JogadorItem.objects.get(pk=int(desafio['item']))
-                                if jogador_item.jogador == jogadorFinded:
-                                    if jogador_item.quantidade_bloqueada == jogador_item.quantidade:
-                                        return Response({"error": True, "message": "Você esta com esse item bloqueado, ou seja, já esta usando em outro desafio."}, status=400)
+                            if desafio['item']:
+                                if JogadorItem.objects.get(pk=int(desafio['item'])):
+                                    jogador_item = JogadorItem.objects.get(pk=int(desafio['item']))
+                                    if jogador_item.jogador == jogadorFinded:
+                                        if jogador_item.quantidade_bloqueada == jogador_item.quantidade:
+                                            return Response({"error": True, "message": "Você esta com esse item bloqueado, ou seja, já esta usando em outro desafio."}, status=400)
+                                        else:
+                                            jogador_item.quantidade_bloqueada = jogador_item.quantidade_bloqueada + 1
+                                            jogador_item.save()
+
+                                            desafioFinded.item_desafiado = jogador_item
+                                            desafioFinded.status = 'A'
+                                            desafioFinded.save()
+
+                                            serializer = DesafioSerializer(desafioFinded)
+
+                                            return Response(serializer.data, status=200)
                                     else:
-                                        jogador_item.quantidade_bloqueada = jogador_item.quantidade_bloqueada + 1
-                                        jogador_item.save()
-
-                                        desafioFinded.item_desafiado = jogador_item
-                                        desafioFinded.status = 'A'
-                                        desafioFinded.save()
-
-                                        serializer = DesafioSerializer(desafioFinded)
-
-                                        return Response(serializer.data, status=200)
+                                        return Response({"error": True,
+                                                        "message": "Esse item não pertence a esse jogador."},
+                                                        status=400)
                                 else:
                                     return Response({"error": True,
-                                                     "message": "Esse item não pertence a esse jogador."},
+                                                    "message": "Esse item não existe para esse jogador."},
                                                     status=400)
                             else:
-                                return Response({"error": True,
-                                                 "message": "Esse item não existe para esse jogador."},
-                                                status=400)
+                                desafioFinded.item_desafiado = None
+                                desafioFinded.status = 'A'
+                                desafioFinded.save()
+
+                                serializer = DesafioSerializer(desafioFinded)
+
+                                return Response(serializer.data, status=200)
                         #quando o jogador vai alterar o item do desafio
                         else:
-                            if JogadorItem.objects.get(pk=int(desafio['item'])):
-                                jogador_item = JogadorItem.objects.get(pk=int(desafio['item']))
+                            if desafio['item']:
+                                if JogadorItem.objects.get(pk=int(desafio['item'])):
+                                    jogador_item = JogadorItem.objects.get(pk=int(desafio['item']))
 
-                                if jogador_item.jogador == jogadorFinded:
-                                    if jogador_item.quantidade_bloqueada == jogador_item.quantidade:
-                                        return Response({"error": True,
-                                                         "message": "Você esta com esse item bloqueado, ou seja, já esta usando em outro desafio."},
-                                                        status=400)
+                                    if jogador_item.jogador == jogadorFinded:
+                                        if jogador_item.quantidade_bloqueada == jogador_item.quantidade:
+                                            return Response({"error": True,
+                                                            "message": "Você esta com esse item bloqueado, ou seja, já esta usando em outro desafio."},
+                                                            status=400)
+                                        else:
+                                            jogador_item.quantidade_bloqueada = jogador_item.quantidade_bloqueada + 1
+                                            jogador_item.save()
+
+                                            jogador_item_old = JogadorItem.objects.get(pk=desafioFinded.item_desafiado.id)
+                                            jogador_item_old.quantidade_bloqueada = jogador_item_old.quantidade_bloqueada - 1
+                                            jogador_item_old.save()
+
+                                            desafioFinded.item_desafiado = jogador_item
+                                            desafioFinded.status = 'A'
+                                            desafioFinded.save()
+
+                                            serializer = DesafioSerializer(desafioFinded)
+
+                                            return Response(serializer.data, status=200)
                                     else:
-                                        jogador_item.quantidade_bloqueada = jogador_item.quantidade_bloqueada + 1
-                                        jogador_item.save()
-
-                                        jogador_item_old = JogadorItem.objects.get(pk=desafioFinded.item_desafiado.id)
-                                        jogador_item_old.quantidade_bloqueada = jogador_item_old.quantidade_bloqueada - 1
-                                        jogador_item_old.save()
-
-                                        desafioFinded.item_desafiado = jogador_item
-                                        desafioFinded.status = 'A'
-                                        desafioFinded.save()
-
-                                        serializer = DesafioSerializer(desafioFinded)
-
-                                        return Response(serializer.data, status=200)
+                                        return Response({"error": True,
+                                                        "message": "Esse item não pertence a esse jogador."},
+                                                        status=400)
                                 else:
                                     return Response({"error": True,
-                                                     "message": "Esse item não pertence a esse jogador."},
+                                                    "message": "Esse item não existe para esse jogador."},
                                                     status=400)
                             else:
-                                return Response({"error": True,
-                                                 "message": "Esse item não existe para esse jogador."},
-                                                status=400)
+                                desafioFinded.item_desafiado = None
+                                desafioFinded.status = 'A'
+                                desafioFinded.save()
+
+                                serializer = DesafioSerializer(desafioFinded)
+
+                                return Response(serializer.data, status=200)
                     elif jogadorFinded == desafioFinded.desafiante:
                         # quando o jogador vai adicionar o item no desafio pela primeira vez
                         if not desafioFinded.item_desafiante:
-                            if JogadorItem.objects.get(pk=int(desafio['item'])):
-                                jogador_item = JogadorItem.objects.get(pk=int(desafio['item']))
-                                if jogador_item.jogador == jogadorFinded:
-                                    if jogador_item.quantidade_bloqueada == jogador_item.quantidade:
-                                        return Response({"error": True,
-                                                         "message": "Você esta com esse item bloqueado, ou seja, já esta usando em outro desafio."},
-                                                        status=400)
+                            if desafio['item']:
+                                if JogadorItem.objects.get(pk=int(desafio['item'])):
+                                    jogador_item = JogadorItem.objects.get(pk=int(desafio['item']))
+                                    if jogador_item.jogador == jogadorFinded:
+                                        if jogador_item.quantidade_bloqueada == jogador_item.quantidade:
+                                            return Response({"error": True,
+                                                            "message": "Você esta com esse item bloqueado, ou seja, já esta usando em outro desafio."},
+                                                            status=400)
+                                        else:
+                                            jogador_item.quantidade_bloqueada = jogador_item.quantidade_bloqueada + 1
+                                            jogador_item.save()
+
+                                            desafioFinded.item_desafiante = jogador_item
+                                            desafioFinded.save()
+
+                                            serializer = DesafioSerializer(desafioFinded)
+
+                                            return Response(serializer.data, status=200)
                                     else:
-                                        jogador_item.quantidade_bloqueada = jogador_item.quantidade_bloqueada + 1
-                                        jogador_item.save()
-
-                                        desafioFinded.item_desafiante = jogador_item
-                                        desafioFinded.status = 'A'
-                                        desafioFinded.save()
-
-                                        serializer = DesafioSerializer(desafioFinded)
-
-                                        return Response(serializer.data, status=200)
+                                        return Response({"error": True,
+                                                        "message": "Esse item não pertence a esse jogador."},
+                                                        status=400)
                                 else:
                                     return Response({"error": True,
-                                                     "message": "Esse item não pertence a esse jogador."},
+                                                    "message": "Esse item não existe para esse jogador."},
                                                     status=400)
                             else:
-                                return Response({"error": True,
-                                                 "message": "Esse item não existe para esse jogador."},
-                                                status=400)
+                                desafioFinded.item_desafiante = None
+                                desafioFinded.save()
+                                serializer = DesafioSerializer(desafioFinded)
+
+                                return Response(serializer.data, status=200)
                         # quando o jogador vai alterar o item do desafio
                         else:
-                            if JogadorItem.objects.get(pk=int(desafio['item'])):
-                                jogador_item = JogadorItem.objects.get(pk=int(desafio['item']))
+                            if desafio['item']:
+                                if JogadorItem.objects.get(pk=int(desafio['item'])):
+                                    jogador_item = JogadorItem.objects.get(pk=int(desafio['item']))
 
-                                if jogador_item.jogador == jogadorFinded:
-                                    if jogador_item.quantidade_bloqueada == jogador_item.quantidade:
-                                        return Response({"error": True,
-                                                         "message": "Você esta com esse item bloqueado, ou seja, já esta usando em outro desafio."},
-                                                        status=400)
+                                    if jogador_item.jogador == jogadorFinded:
+                                        if jogador_item.quantidade_bloqueada == jogador_item.quantidade:
+                                            return Response({"error": True,
+                                                            "message": "Você esta com esse item bloqueado, ou seja, já esta usando em outro desafio."},
+                                                            status=400)
+                                        else:
+                                            jogador_item.quantidade_bloqueada = jogador_item.quantidade_bloqueada + 1
+                                            jogador_item.save()
+
+                                            jogador_item_old = JogadorItem.objects.get(pk=desafioFinded.item_desafiante.id)
+                                            jogador_item_old.quantidade_bloqueada = jogador_item_old.quantidade_bloqueada - 1
+                                            jogador_item_old.save()
+
+                                            desafioFinded.item_desafiante = jogador_item
+                                            desafioFinded.save()
+
+                                            serializer = DesafioSerializer(desafioFinded)
+
+                                            return Response(serializer.data, status=200)
                                     else:
-                                        jogador_item.quantidade_bloqueada = jogador_item.quantidade_bloqueada + 1
-                                        jogador_item.save()
-
-                                        jogador_item_old = JogadorItem.objects.get(pk=desafioFinded.item_desafiante.id)
-                                        jogador_item_old.quantidade_bloqueada = jogador_item_old.quantidade_bloqueada - 1
-                                        jogador_item_old.save()
-
-                                        desafioFinded.item_desafiante = jogador_item
-                                        desafioFinded.save()
-
-                                        serializer = DesafioSerializer(desafioFinded)
-
-                                        return Response(serializer.data, status=200)
+                                        return Response({"error": True,
+                                                        "message": "Esse item não pertence a esse jogador."},
+                                                        status=400)
                                 else:
                                     return Response({"error": True,
-                                                     "message": "Esse item não pertence a esse jogador."},
+                                                    "message": "Esse item não existe para esse jogador."},
                                                     status=400)
                             else:
-                                return Response({"error": True,
-                                                 "message": "Esse item não existe para esse jogador."},
-                                                status=400)
+                                desafioFinded.item_desafiante = None
+                                desafioFinded.save()
+                                serializer = DesafioSerializer(desafioFinded)
 
+                                return Response(serializer.data, status=200)
                     else:
                         return Response({"error": True,
                                          "message": "Você não é nem o desafiante e nem o desafiado."},
