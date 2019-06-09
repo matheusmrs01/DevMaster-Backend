cb15758d8ceb8274ed72a8a01d1c975f8fafe0fa
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-06-08 22:04:31 -0300

    feat: endpoint para receber a requisição do gitlab

--- missao/views.py
+++ missao/views.py
@@ -1,6 +1,9 @@
 import base64
 import imghdr
 import json
+import requests
+from datetime import datetime
+import time
 
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
@@ -12,7 +15,134 @@ from rest_framework import permissions
 from rest_framework.renderers import JSONRenderer
 
 from missao.api.serializers import GrupoSerializer, MissaoSerializer
+
 from missao.models import Grupo, Missao
+from jogador.models import Jogador, XpEvento
+from desafio.models import DesafioMissoes
+from evento.models import Evento
+from django.contrib.auth.models import User
+
+class IssueGitlabViewSet(GenericViewSet):
+    queryset = Missao.objects.all()
+    serializer_class = MissaoSerializer
+
+    @method_decorator(csrf_exempt)
+    @action(methods=['POST'], detail=False, url_path='gitlabissue')
+    def CriarMissao(self, request):
+        gitlab_token = request.META['HTTP_X_GITLAB_TOKEN']
+        if (request.META['CONTENT_TYPE'] == 'application/json'):
+            jsonData = json.loads(request.body)
+
+            issue = jsonData
+        else:
+            issue = request.POST.get('issue', '')
+
+        if gitlab_token == '8ae957d6f68f46055091ff319fa67b888e122a3a':
+            if issue['object_kind'] == 'issue':
+                userFinded = User.objects.filter(username=issue['assignee']['username'])
+                if userFinded:
+                    jogadorFinded = Jogador.objects.filter(user=User.objects.get(username=issue['assignee']['username']))
+                    #Função usada para criar uma missão
+                    if jogadorFinded:
+                        jogadorFinded = Jogador.objects.get(user=User.objects.get(username=issue['assignee']['username']))
+                        if issue['object_attributes']['action'] == 'open' and issue['object_attributes']['state'] == 'opened':
+                            newMissao = Missao()
+                            newMissao.jogador = jogadorFinded
+                            newMissao.nome_missao = issue['object_attributes']['title']
+                            newMissao.xp_missao = 80
+
+                            if issue['object_attributes']['due_date']:
+                                newMissao.data = issue['object_attributes']['due_date']
+                            
+                            newMissao.id_issue = issue['object_attributes']['id']
+                            newMissao.id_projeto = issue['object_attributes']['project_id']
+
+                            if issue['object_attributes']['milestone_id']:
+                                newMissao.id_milestone = issue['object_attributes']['milestone_id']
+                                if not Grupo.objects.filter(id_milestone=issue['object_attributes']['milestone_id']):
+                                    newGrupo = Grupo()
+                                    newGrupo.name = 'Milestone ' + str(issue['object_attributes']['milestone_id'])
+                                    newGrupo.id_milestone = issue['object_attributes']['milestone_id']
+                                    newGrupo.xp_grupo = 10
+                                    newGrupo.save()
+                                else:
+                                    grupoFinded = Grupo.objects.get(id_milestone=issue['object_attributes']['milestone_id'])
+                                    grupoFinded.xp_grupo = grupoFinded.xp_grupo + 10
+                                    grupoFinded.save()
+
+                            newMissao.save()
+                            return Response({'Missão cadastrada com sucesso!'})
+                        
+                        #finaliza a missão ja criada
+                        if issue['object_attributes']['state'] == 'closed':
+                            missaoFinded = Missao.objects.filter(id_issue=issue['object_attributes']['id'])
+                            if(missaoFinded):
+                                missaoFinded = Missao.objects.get(id_issue=issue['object_attributes']['id'])
+                                if missaoFinded.status == False:
+                                    r = requests.get('https://gitlab.com/api/v4/projects/'+str(issue['object_attributes']['project_id'])+'/issues/'+str(issue['object_attributes']['id'])+'?private_token='+str(jogadorFinded.private_token))
+                                    issue = r.json()
+                                    xpMissao = 80
+
+                                    #verifica a data da missão e a data que vai ser finalizada
+                                    if issue['due_date']:
+                                        missaoFinded.data = issue['due_date']
+                                        if datetime.now() > datetime.strptime(issue['due_date'].replace('-','/'), '%Y/%m/%d'):
+                                            xpMissao = xpMissao - 20
+                                        else:
+                                            missaoFinded.nice_data = True
+                                    
+                                    #verifica se foi fechada no tempo certo
+                                    if issue['time_stats']['time_estimate']:
+                                        if not issue['time_stats']['total_time_spent']:
+                                            xpMissao = xpMissao - 50
+                                        else:
+                                            if issue['time_stats']['time_estimate'] > issue['time_stats']['total_time_spent']:
+                                                xpMissao = xpMissao - 50
+                                            else:
+                                                missaoFinded.nice_tempo = True
+
+                                    #Verifica se tem evento, para poder atualizar o valor da xp
+                                    eventosAtivo = Evento.objects.filter(is_active=True)    
+                                    if eventosAtivo:
+                                        for eventoAtivo in eventosAtivo:
+                                            xpMissao = xpMissao * eventoAtivo.multiplicador_xp
+                                            if XpEvento.objects.filter(jogador=jogadorFinded,evento=eventoAtivo):
+                                                xpEventoFinded = XpEvento.objects.get(jogador=jogadorFinded,evento=eventoAtivo)
+                                                xpEventoFinded.xp_evento = xpEventoFinded.xp_evento + xpMissao
+                                                xpEventoFinded.save()
+                                            else:
+                                                newXpEvento = XpEvento()
+                                                newXpEvento.evento = eventoAtivo
+                                                newXpEvento.jogador = jogadorFinded
+                                                newXpEvento.xp_evento = xpMissao
+                                                newXpEvento.save()
+
+                                    missaoFinded.xp_ganha = xpMissao
+                                    missaoFinded.status = True
+
+                                    #missao desafio - salva o valor em cada missão do desafio
+                                    missoesDesafio = DesafioMissoes.objects.filter(missao=missaoFinded)
+                                    if missoesDesafio:
+                                        for missaoDesafio in missoesDesafio:
+                                            missaoDesafio.xp_ganha = xpMissao
+                                            missaoDesafio.save()
+
+                                    jogadorFinded.xp_total = jogadorFinded.xp_total + xpMissao
+                                    jogadorFinded.save()
+                                    missaoFinded.save()
+                                    return Response({'Missão finalizada com sucesso!'})
+                                else:
+                                    return Response({'Missão já foi fechada.'})
+                            else:
+                                return Response({'Missão nunca foi cadastrada na base de dados.'})
+                    else:
+                        return Response({'Jogador dessa Issue não existe.'})
+                else:
+                    return Response({'Usuario não existe.'})
+            else:
+                return Response({'O objeto em questão não é um issue.'})
+        else:
+            return Response({'Sem autorização.'})
 
 class GrupoViewSet(GenericViewSet):
     queryset = Grupo.objects.all()
